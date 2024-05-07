082c126478dd73c9c76574e01b3054cb7f7fc181
DeeSzubert <pietryd@yahoo.com>
2024-05-07 09:33:52 +0100

    CORE: View an individual article & CORE: View a list of comments associated with an article

--- src/api.js
+++ src/api.js
@@ -11,4 +11,32 @@ const getArticles = () => {
     });
 };
 
-export { getArticles };
+const getArticleById = (article_id) => {
+  if (article_id) {
+    return axios
+      .get(`https://nc-news-project-lymg.onrender.com/articles/${article_id}`)
+      .then(({ data }) => {
+        return data.article;
+      })
+      .catch((error) => {
+        return error.message;
+      });
+  }
+};
+
+const getCommentByArticleById = (article_id) => {
+  if (article_id) {
+    return axios
+      .get(
+        `https://nc-news-project-lymg.onrender.com/articles/${article_id}/comments`
+      )
+      .then(({ data }) => {
+        return data.comments;
+      })
+      .catch((error) => {
+        return error.message;
+      });
+  }
+};
+
+export { getArticles, getArticleById, getCommentByArticleById };
